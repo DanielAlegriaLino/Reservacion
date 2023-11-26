c885bf75cde0da7cda18b97224ef3c147e643342
ElEdMedina21 <emedina21@alumnos.uaq.mx>
2023-11-25 18:28:01 -0600

    added a key fetch to GenerarResevacion

--- WebClient/reservacionScript.js
+++ WebClient/reservacionScript.js
@@ -1,13 +1,42 @@
 document.addEventListener("DOMContentLoaded", ()=>{
     fetch(`http://127.0.0.1:8000/RevisarMesas/2023-11-${localStorage.getItem("date")}`)
     .then(response => {
-        return response.json()
+        return response.json();
     })  
     .then(data => {
-        reservedTables(data)
-        localStorage.clear()
+        reservedTables(data);
     })
+    generateRes()
+
 })
+
+function sendRes(key){
+    // fetch()
+}
+
+function generateRes(){
+    const tables = document.getElementsByClassName("table-img");
+    for (let i = 0; i < tables.length; i++) {
+        tables[i].addEventListener("click", () => {
+            fetch(`http://127.0.0.1:8000/GenerarReservacion/2023-11-${localStorage.getItem("date")}`,{
+                method:'POST'
+            })
+            .then(response => {
+                return response.text()
+            })
+            .then(data => {
+                localStorage.setItem('key',data)
+            })
+        }); 
+    }
+}
+
+function clearLocal(){
+    const home = document.querySelector('.navbar-brand');
+    home.addEventListener('click', () => {
+        localStorage.clear();
+    })
+}
 
 function reservedTables(arr){
     const tables = document.getElementsByClassName("mesa")
