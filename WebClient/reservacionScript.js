154ab39420da1183489fa5844573a9879e19c0de
ObamaBolsa <daniel.alegria.lino@gmail.com>
2023-11-27 18:46:14 -0600

    fetches to api for block tables

--- WebClient/reservacionScript.js
+++ WebClient/reservacionScript.js
@@ -18,6 +18,7 @@ function generateRes(){
     const tables = document.getElementsByClassName("table-img");
     for (let i = 0; i < tables.length; i++) {
         tables[i].addEventListener("click", () => {
+            localStorage.setItem('table',i)
             fetch(`http://127.0.0.1:8000/GenerarReservacion/2023-11-${localStorage.getItem("date")}`,{
                 method:'POST'
             })
@@ -53,3 +54,23 @@ function reservedTables(arr){
         }
     }
 } 
+
+const btn_establecerReservacion = document.getElementById("btn_EstablecerReservacion")
+btn_establecerReservacion.addEventListener("click", () => establecerReservacion())
+function establecerReservacion(){
+    let mesa_reservacion = localStorage.getItem('table')
+    let id_reservacion = localStorage.getItem('key')
+    let date_reservacion = `2023-11-${localStorage.getItem("date")}`
+
+    //fetch( ) -> hacer fetch y ver si se logro hacer la reservacion, si no retornar false   
+    console.log(id_reservacion)
+    console.log(date_reservacion)
+    console.log(mesa_reservacion)
+    console.log(1231234)
+    let url =  `http://127.0.0.1:8000/EstablecerReservacion/${date_reservacion}/${id_reservacion}/${mesa_reservacion}`
+    alert(url)
+    fetch(url)
+    .then(response => response.json())
+    .then(response => console.log(response))
+    location.reload(); 
+}
